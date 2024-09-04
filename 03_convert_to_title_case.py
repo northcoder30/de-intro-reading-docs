39902b2691b52ec62b2eaaf3784cf3ab2dc7091e
northcoder30 <crthomas37@yahoo.com>
2024-09-04 23:25:18 +0530

    04-09-24 Convert to title case

--- 03_convert_to_title_case.py
+++ 03_convert_to_title_case.py
@@ -1,8 +1,10 @@
 from test_api.checks import run_test, skip_test, format_err_msg
-
+import string
 
 def convert_to_title_case(sentence):
-    pass
+    cap = string.capwords(sentence, sep = None)
+    return cap
+    # return re.sub(r"[A-Za-z]+('[A-Za-z] +)?", lambda word: word.group(0).capitalize(),sentence)
 
 
 @run_test
@@ -11,7 +13,7 @@ def test_convert_single_word_to_title_case():
         format_err_msg("Hi", convert_to_title_case("hi"))
 
 
-@skip_test
+@run_test
 def test_convert_multi_word_to_title_case():
     assert convert_to_title_case("hello world") == "Hello World", \
         format_err_msg("Hello World", convert_to_title_case("hello world"))
@@ -25,7 +27,7 @@ def test_convert_multi_word_to_title_case():
                        convert_to_title_case("Well ain't this awkward"))
 
 
-@skip_test
+@run_test
 def test_convert_complex_sentence_to_title_case():
     assert convert_to_title_case(
         "not just apostrophes, could be something-else") \
